0c66264089f551ce5005d54b771baf83bd830adc
Zakariye Mohamud Nor <154254662+Zak937@users.noreply.github.com>
2026-05-02 12:18:54 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/splash_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const LostAndFoundApp());
+}
+
+class LostAndFoundApp extends StatelessWidget {
+  const LostAndFoundApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lost & Found SIMAD',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1A73E8),
+          brightness: Brightness.light,
+        ),
+        fontFamily: 'Poppins',
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
